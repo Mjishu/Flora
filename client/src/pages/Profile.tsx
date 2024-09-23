40e68b3bb31d8d211ee59ee4bfda5f71945b5e41
mjishu <128941980+Mjishu@users.noreply.github.com>
2024-09-22 22:20:36 -0400

    outline for user profile completed

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React from 'react';
+import { UseUser } from '../components/user/userContext';
+import style from "../styles/profile.module.css";
 
 function Profile() {
+    const { currentUser, userLoading } = UseUser();
+    const [showTimezone, setShowTimezone] = React.useState(false);
+    const [timezones, setTimezones] = React.useState(null);
+    const [newTimezone, setNewTimezone] = React.useState("") //* change this maybe to include other user options? or curentUser.timezone?
+
+    if (userLoading) {
+        return <h1>Loading...</h1>
+    }
+
+    React.useEffect(() => {
+        fetch("/api/users/timezones")
+            .then(res => res.json())
+            .then(data => setTimezones(data))
+            .catch(err => console.error(`There was an error trying to fetch timezones: ${err}`))
+    }, [])
+
+    function updateUser() {
+        const token = localStorage.getItem("token");
+        if (!token || token === null) { return console.error("Invalid token") }
+        const fetchParams = {
+            method: "POST",
+            headers: { "Content-Type": "application/json", Authorization: token },
+            body: JSON.stringify(newTimezone)
+        }
+    }
+
+    function updateTimezone(timezone: unknown) {
+        setNewTimezone(timezone)
+    }
+
+    const mappedTimezones = timezones.map(zone => {
+        return <div className={style.zone} onClick={() => updateTimezone(zone)}>
+            <h5>{zone.name}</h5>
+        </div >
+    })
+
     return (
-        <div>Profile</div>
+        <div>
+            <h1>Welcome {currentUser.username}</h1>
+            <div>
+                <button class={style.userTimezones} onClick={() => setShowTimezone(prev => !prev)}>Timezone</button>
+                <div className={style.timezoneHolder}>
+                    {showTimezone && mappedTimezones}
+                </div>
+            </div>
+        </div>
     )
 }
 
